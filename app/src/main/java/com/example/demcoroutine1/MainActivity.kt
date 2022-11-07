aa2b42fbd204bb72fd1503a48dad589cd2322735
test test <test@test.ma>
2022-11-07 10:44:43 +0000

    version avec async

--- app/src/main/java/com/example/demcoroutine1/MainActivity.kt
+++ app/src/main/java/com/example/demcoroutine1/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.demcoroutine1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import kotlinx.coroutines.*
 import kotlin.concurrent.thread
@@ -10,46 +12,38 @@ import kotlin.concurrent.thread
 class MainActivity : AppCompatActivity() {
     val montag:String="MonTag"
     lateinit var txt:TextView
+    lateinit var btn:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         txt=findViewById(R.id.txt) as TextView
+        btn=findViewById(R.id.btn) as Button
+        btn.setOnClickListener{
+            val int =Intent(applicationContext,testActivity::class.java)
+            startActivity(int)
+        }
         Log.i(montag, Thread.currentThread().name)
         GlobalScope.launch(Dispatchers.IO) {
-            Log.i(montag, "Coroutine")
+            Log.i(montag, "Coroutine1")
+            fctlourde()
 
-            var data1= async { fctlourde() }
-            var data2=async{fcttreslourde()}
-            //pour exécuter ces deux méthodes en parallèle
-            //le retour n'est pas un String c'est un deferred String
-            //c'est pourquoi on a besoin d'utiliser await
-            withContext(Dispatchers.Main)
-            {
-                val txt1=findViewById(R.id.txt) as TextView
-                txt1.text = data1.await()
-            }
-            withContext(Dispatchers.Main)
-            {
-                val txt2 = findViewById(R.id.txt) as TextView
-                txt2.text = data2.await()
-            }
+
         }
-       Log.i(montag, "Je suis thread main")
+
+
+
+        Log.i(montag, "Je suis thread main")
 txt.text="Hello main"
     }
-    suspend fun fctlourde():String
-    {
+    suspend fun fctlourde() {
         //on va essayer de simuler une action lourde qui a besoin de
         //10s pour s'exécuter
         // en retardant la coroutine
-        delay(10000L)
+        for (i in 1..1000)
+        {    delay(1000L)
         //delay ne peut être utilisée qu'avec des méthode suspend
-        return "Enfin j'ai fini fct lourde"
+        Log.i(montag,"Tour : " + i)
+        }
     }
-    suspend fun fcttreslourde():String
-    {
 
-        delay(20000L)
-        return "Enfin j'ai fini la fonction très lourde"
-    }
 }
